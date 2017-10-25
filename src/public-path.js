cfa6a53ef08c36d59a8807014871d1241cc64e33
Yann Thomas-Gérard <inside@gmail.com>
2017-10-25 09:24:59 +0200

    add public path if not in dev  mode

--- src/public-path.js
+++ src/public-path.js
@@ -1,4 +1,8 @@
 import { appConfig } from 'roc-package-web-app-react/app/shared/universal-config';
 
 // __webpack_public_path__ = '//asdfcdn.dev/';
-__webpack_public_path__ = appConfig.ASSETS_PUBLIC_PATH;
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+
+if (process.env.NODE_ENV !== 'development') {
+  __webpack_public_path__ = appConfig.ASSETS_PUBLIC_PATH;
+}
