9351837bc476d18587ff6fc3a3128a87d9428689
Yann Thomas-Gérard <inside@gmail.com>
2017-10-25 11:25:02 +0200

    remove debug

--- src/public-path.js
+++ src/public-path.js
@@ -1,8 +1,5 @@
 import { appConfig } from 'roc-package-web-app-react/app/shared/universal-config';
 
-// __webpack_public_path__ = '//asdfcdn.dev/';
-console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
-
 if (process.env.NODE_ENV !== 'development') {
   __webpack_public_path__ = appConfig.ASSETS_PUBLIC_PATH;
 }
